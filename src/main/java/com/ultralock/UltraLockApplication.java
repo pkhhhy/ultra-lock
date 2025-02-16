fa6327f0244c20025deca9a765428882e24f2245
裴克辉 <1274969770@qq.com>
2025-02-16 13:10:47 +0800

    feat: init project

---
+++ src/main/java/com/ultralock/UltraLockApplication.java
@@ -0,0 +1,13 @@
+package com.ultralock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltraLockApplication.class, args);
+    }
+
+}
